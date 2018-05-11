b994d961473f5cd2bd205b2b7a4628e86dcdff9a
Daniel Hamidi <hamidiwebdesign@gmail.com>
2018-05-11 09:46:46 -0700

    moving the ball from center

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -5,14 +5,29 @@ export default class Ball {
         this.boardWidth = boardWidth;
         this.boardHeight = boardHeight;
         this.direction = 1;
+
+        this.reset(); //calling the reset
     }
+
+    //==========================================
+    reset() { //reset ball
+        this.x = this.boardWidth / 2;
+        this.y = this.boardHeight / 2;
+        this.vy = Math.floor(Math.random() * 10 - 5);
+        this.vx = this.direction * (6 - Math.abs(this.vy));
+    }
+
+    //==========================================
     render(svg, player1, player2) {
-        //creating Ball elements=============================
+        this.x += this.vx;
+        this.y += this.vy;
+
+        //Ball elements=============================
 
         let circle = document.createElementNS(SVG_NS, 'circle');
         circle.setAttributeNS(null, 'r', this.radius);
-        circle.setAttributeNS(null, 'cx', this.boardWidth / 2); //x of the center point
-        circle.setAttributeNS(null, 'cy', this.boardHeight / 2); // y of the center point
+        circle.setAttributeNS(null, 'cx', this.x); //x of the center point
+        circle.setAttributeNS(null, 'cy', this.y); // y of the center point
         circle.setAttributeNS(null, 'fill', 'white'); //x of the top left corner
 
         svg.appendChild(circle);
